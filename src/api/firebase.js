8086b5d1a44bcd803799643a0b116c0293938f6e
Tiago Carnevalli <carnevalli@itstransdata.com>
2022-09-05 16:31:58 -0300

    Container configurado

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from 'firebase/firestore';
 
-// import { getStorage  } from 'firebase/storage';
-
 // Your web app's Firebase configuration
 const firebaseConfig = initializeApp({
-  apiKey: "AIzaSyCvd16xyBrISnfefW4GhOXtQeN-SVqUipE",
-  authDomain: "secret-family.firebaseapp.com",
-  projectId: "secret-family",
-  storageBucket: "secret-family.appspot.com",
-  messagingSenderId: "564717251448",
-  appId: "1:564717251448:web:8992cd88a477de7230f68d"
+  apiKey: 'AIzaSyCvd16xyBrISnfefW4GhOXtQeN-SVqUipE',
+  authDomain: 'secret-family.firebaseapp.com',
+  projectId: 'secret-family',
+  storageBucket: 'secret-family.appspot.com',
+  messagingSenderId: '564717251448',
+  appId: '1:564717251448:web:8992cd88a477de7230f68d'
 });
 
 // Initialize Firebase
 export const dbFirestore = getFirestore(firebaseConfig);
-// export const dbStorage = getStorage(app);
+export const auth = getAuth(firebaseConfig);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,62 @@
 import React, { useState } from 'react';
 import '../styles/App.scss';
 
+import { signInWithEmailAndPassword, signOut,  } from 'firebase/auth';
+import { auth } from '../api/firebase';
+
 function App() {
   const [email, setEmail] = useState('');
-  const [password, setpassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [hide, setHide] = useState(false);
+  const [user, setUser] = useState({});
+
+  function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userLogged) => {
+        console.log('Logged:');
+        console.log(userLogged.user);
+        console.log(userLogged.providerId);
+        console.log(userLogged.operationType);
+        setUser(userLogged.user);
+      })
+      .catch((error) => {
+        console.log('Error:');
+        console.log(error);
+      })
+  }
   
   return (
     <div className='login-container'>
-      <form>
-        <label>
-          Email
-        </label>
-        <input id='email' type='email' placeholder='Email' value={email} onChange={({currentTarget}) => { setEmail(currentTarget.value.replace(/\s/g, '')) }} />
-        <input id='password' type={'password'} placeholder='Senha' />
+      <header>
+        <h2>Faça o seu login</h2>
+      </header>
+      <form onSubmit={(form) => handleLogin(form)}>
+        <div>
+          <label>Email</label>
+          <input id='email' type='email' placeholder='exemplo@email.com' value={email} onChange={({target}) => { setEmail(target.value) }} />
+        </div>
+        <div>
+          <label>Senha</label>
+          <input id='password' type={hide ? 'password' : 'text'} placeholder='Senha' value={password} onChange={({target}) => { setPassword(target.value) }}  />
+          <input type='checkbox' checked={hide} onChange={() => { setHide(!hide) }} style={{ width: 'fit-content' }} />
+        </div>
+        <a href='www.google.com'>Esqueci minha senha</a>
         <button type='submit'>Entrar</button>
       </form>
+      <div style={{ marginTop: '0.4rem' }}>
+        <button onClick={() => { setUser({}); signOut(auth) }} 
+          style={{ 
+            background: 'var(--red)', 
+            border: 'none', 
+            color: 'white', 
+            padding: '0.2rem 0.4rem', 
+            borderRadius: '0.2rem',
+            marginRight: '0.4rem'
+          }}>Sair</button>
+        {(user as any).email || '-'}
+      </div>
     </div>
   );
 }

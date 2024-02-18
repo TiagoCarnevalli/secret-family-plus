f8c7645ac9a2dae76406b89e91b1b2eeac6c1c97
TiagoCarnevalli <tiagocelias2005@hotmail.com>
2024-02-17 21:58:17 -0300

    Desenvolvimento do filtro

--- src/components/InfoTable/index.tsx
+++ src/components/InfoTable/index.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import './styles.scss'
 import { DocumentData, collection, getDocs, query } from 'firebase/firestore';
 import { dbFirestore } from '../../api/firebase';
+import LabeledInput from '../LabeledInput';
 
 export default function InfoTable() {
-    const [list, setList] = useState<DocumentData[]>()
+    const [list, setList] = useState<DocumentData[]>();
+    const [search, setSearch] = useState('');
 
     async function getList() {
         const { docs } = await getDocs(query(collection(dbFirestore, 'participantes')));
@@ -15,20 +17,28 @@ export default function InfoTable() {
         getList();
     }, [])
 
-    return <div className='table-container'><table className='gifts-table'>
-        <thead>
-            <tr>
-                <th style={{ minWidth: '14rem' }}>Nome</th>
-                <th>Presentes</th>
-            </tr>
-        </thead>
-        <tbody>
-            {list?.sort((a, b) => { return a.name > b.name ? 1 : -1 }).map((item, index) => {
-                return <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.giftList}</td>
-                </tr>
-            })}
-        </tbody>
-    </table></div>
+    return <div>
+        <div className='table-container'>
+            <table className='gifts-table'>
+                <thead>
+                    <tr>
+                        <th style={{ minWidth: '14rem' }}>Nome</th>
+                        <th>Presentes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list?.sort((a, b) => { return a.name > b.name ? 1 : -1 }).filter((item) => { return item.name.toLowerCase().includes(search.toLocaleLowerCase()) }).map((item, index) => {
+                        return <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>{item.giftList}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+
+        <div className='search-input'>
+            <LabeledInput label={'Pesquisar por nome'} value={search} type={'text'} onChange={(e) => setSearch(e.currentTarget.value)} />
+        </div>
+    </div>
 }
